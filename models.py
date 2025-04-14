6179ff5a58445244acf62c39badd98280c4afd70
Pavel Patsey <patsey.pavel@gmail.com>
2025-04-14 18:21:29 +0300

    Make migration Modify strings in tasks table

--- models.py
+++ models.py
@@ -1,13 +1,15 @@
-from sqlalchemy import JSON, Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import JSON, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    pass
 
 
 class Task(Base):
     __tablename__ = "tasks"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    car_id = Column(String)
-    status = Column(String)
-    messages = Column(JSON, default=[])
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String(30), nullable=False)
+    car_id = mapped_column(String(30), nullable=False)
+    status = mapped_column(String(30), nullable=False)
+    messages = mapped_column(JSON, default=[])
