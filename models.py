8515ed44f3a97d151b1e77b77f703911e2cf1c45
Pavel Patsey <patsey.pavel@gmail.com>
2025-05-01 14:29:57 +0300

    Add message table

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import JSON, String
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import ForeignKey, String, Text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
 class Base(DeclarativeBase):
@@ -8,8 +8,17 @@ class Base(DeclarativeBase):
 
 class Task(Base):
     __tablename__ = "tasks"
+
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String(30), nullable=False)
-    car_id = mapped_column(String(30), nullable=False)
-    status = mapped_column(String(30), nullable=False)
-    messages = mapped_column(JSON, default=[])
+    car_id: Mapped[str] = mapped_column(String(30), nullable=False)
+    status: Mapped[str] = mapped_column(String(30), nullable=False)
+    messages: Mapped[list["Message"]] = relationship(back_populates="task")
+
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    task_id: Mapped[int] = mapped_column(ForeignKey("tasks.id"))
+    task: Mapped["Task"] = relationship(back_populates="messages")
