0f9ad099df9a7d0ceb9dba7b1997b02a8662dba8
Pavel Patsey <patsey.pavel@gmail.com>
2025-07-24 14:45:52 +0300

    Add test read task by id

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -2,7 +2,7 @@ from fastapi import status
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 
-from app import models
+from app import models, schemas
 from app.schemas import TaskStatuses
 
 
@@ -27,3 +27,24 @@ def test_read_tasks(client: TestClient, session: Session):
     assert len(data["tasks"]) == 2
     assert data["tasks"][0]["name"] == "test task 2"
     assert data["tasks"][1]["name"] == "test task 1"
+
+
+def test_read_task_by_id(client: TestClient, session: Session):
+    task_in = schemas.TaskCreate(
+        name="test task 1",
+        car_id="car_1",
+        status=TaskStatuses.in_progress,
+    )
+    task = models.Task(**task_in.model_dump())
+    session.add(task)
+    session.commit()
+
+    response = client.get("/tasks/1")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        "id": 1,
+        "name": "test task 1",
+        "car_id": "car_1",
+        "status": "in progress",
+        "messages": [],
+    }
