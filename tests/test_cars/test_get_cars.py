3163c9a05f320bfbdb6be0698a48e01b0aaa00fe
Pavel Patsey <patsey.pavel@gmail.com>
2025-07-24 17:32:35 +0300

    Init cars tests

---
+++ tests/test_cars/test_get_cars.py
@@ -0,0 +1,14 @@
+from fastapi import status
+from fastapi.testclient import TestClient
+
+
+def test_root(client: TestClient):
+    response = client.get("/cars")
+    assert response.status_code == status.HTTP_200_OK
+    response_json = response.json()
+    assert response_json["cars"] == [
+        {"car_id": "car_1", "problems": [], "status": "ok"},
+        {"car_id": "car_2", "problems": [], "status": "ok"},
+        {"car_id": "car_3", "problems": [], "status": "ok"},
+        {"car_id": "car_4", "problems": [], "status": "ok"},
+    ]
