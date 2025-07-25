1ffa93f62f34cb5f0428352d55c536d6c0114b5e
Pavel Patsey <patsey.pavel@gmail.com>
2025-07-25 16:35:06 +0300

    Use fake GarageClient in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,15 @@ from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
-from app.dependencies import get_session
+from app.dependencies import get_garage_client, get_session
+from app.garage import GarageClient
 from app.main import app
 from app.models import Base
+
+
+class FakeGarageClient(GarageClient):
+    UPDATE_STATUS_PROBABILITY = 100
+    SLEEP_DURATION = 0
 
 
 @pytest.fixture(name="session")
@@ -23,7 +29,11 @@ def client_fixture(session: Session):
     def get_session_override():
         return session
 
+    def get_garage_client_override():
+        return FakeGarageClient()
+
     app.dependency_overrides[get_session] = get_session_override
+    app.dependency_overrides[get_garage_client] = get_garage_client_override
 
     client = TestClient(app)
     yield client
