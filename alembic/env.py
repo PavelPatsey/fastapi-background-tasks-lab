7232b31a4e06b4cfa2b8b9f9b785fdcdce2adda8
Pavel Patsey <patsey.pavel@gmail.com>
2025-07-25 12:54:01 +0300

    Fix alembic migration file

--- alembic/env.py
+++ alembic/env.py
@@ -18,8 +18,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 
-import models
-import settings
+from app import models, settings
 
 target_metadata = models.Base.metadata
 
